016dd99d2b71b158014a20bc245cda6ef3bbcf30
Angel <angel.l.puente@gmail.com>
2024-02-01 15:31:43 -0600

    update project text

--- src/app/page.js
+++ src/app/page.js
@@ -106,7 +106,7 @@ export default function Home() {
             projects
           </h2>
           <hr></hr>
-          <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+          <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 text-white">
             {projectGrid}
           </div>
         </section>
